81c235151402274f0d7f06f589eaa104f0c0758a
Ghislain de Juvigny <gdjuvigny@gmail.com>
2014-04-11 16:31:25 +0200

    add node

--- modules/people/manifests/ghislaindj/applications.pp
+++ modules/people/manifests/ghislaindj/applications.pp
@@ -15,4 +15,12 @@ class people::ghislaindj::applications {
   include zsh
   include ohmyzsh
   include imagemagick
+
+  include virtualbox
+  include tunnelblick::beta
+
+  include nodejs::v0_10
+  nodejs::module { 'bower':
+    node_version => 'v0.10'
+  }
 }
