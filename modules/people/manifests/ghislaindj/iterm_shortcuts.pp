58492f8f9724e54bc7956080b6c5bbba6bcbebf7
ghislaindj <gdjuvigny@gmail.com>
2014-04-11 09:36:10 +0200

    Create iterm_shortcuts.pp

---
+++ modules/people/manifests/ghislaindj/iterm_shortcuts.pp
@@ -0,0 +1,8 @@
+class people::ghislaindj::iterm_shortcuts {
+  shortcut {
+    "iTerm Full Screen":
+      app  => "com.googlecode.iterm2",
+      key  => "cmd-&",
+      menu => "Toggle Full Screen";
+  }
+}
