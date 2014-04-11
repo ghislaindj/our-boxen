c68c1fc8ffae99d9ae9648d9000679d858b46377
ghislaindj <gdjuvigny@gmail.com>
2014-04-11 09:51:17 +0200

    Update gitconfig.pp

--- modules/people/manifests/ghislaindj/gitconfig.pp
+++ modules/people/manifests/ghislaindj/gitconfig.pp
@@ -17,15 +17,15 @@ class people::ghislaindj::gitconfig {
     owner  => $luser
   }
 
-  file { "/Users/${::luser}/bin/brew-services.rb":
-    content => template('/opt/boxen/repo/modules/people/templates/shared/brew-services.rb'),
-    group   => 'staff',
-    owner   => $luser
-  }
-
-  file { "/Users/${::luser}/bin/service":
-    content => template('/opt/boxen/repo/modules/people/templates/shared/service'),
-    group   => 'staff',
-    owner   => $luser
-  }
+#  file { "/Users/${::luser}/bin/brew-services.rb":
+#    content => template('/opt/boxen/repo/modules/people/templates/shared/brew-services.rb'),
+#    group   => 'staff',
+#    owner   => $luser
+#  }
+#
+#  file { "/Users/${::luser}/bin/service":
+#    content => template('/opt/boxen/repo/modules/people/templates/shared/service'),
+#    group   => 'staff',
+#    owner   => $luser
+#  }
 }
