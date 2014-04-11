f6ac69106b68b2a826222770acc7ac3141b04c9a
ghislaindj <gdjuvigny@gmail.com>
2014-04-11 09:37:46 +0200

    Create sublime.pp

---
+++ modules/people/manifests/ghislaindj/sublime.pp
@@ -0,0 +1,29 @@
+class people::ghislaindj::sublime {
+  include sublime_text_3
+  include sublime_text_3::package_control
+
+  file { '/Users/benoit/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings':
+    ensure  => file,
+    content => template("/opt/boxen/repo/modules/people/templates/shared/Preferences.sublime-settings"),
+  }
+
+  sublime_text_3::package { 'Theme - SpaceGray':
+  source => 'kkga/spacegray'
+  }
+
+  sublime_text_3::package { 'SublimePuppet':
+    source => 'russCloak/SublimePuppet'
+  }
+
+  sublime_text_3::package { 'Syntax highlithning for SASS':
+    source => 'P233/Syntax-highlighting-for-Sass'
+  }
+
+  sublime_text_3::package { 'BetterCoffeescript':
+    source => 'aponxi/sublime-better-coffeescript'
+  }
+
+  sublime_text_3::package { "BracketHighlighter":
+    source => 'facelessuser/BracketHighlighter',
+  }
+}
